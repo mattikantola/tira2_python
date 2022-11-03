59b7db68c571e57ba2dfd62c806e79a32ba43c2b
Matti Kantola <matti.kantola@helsinki.fi>
2022-11-03 08:17:14 +0200

    tasajako tehty

---
+++ Viikko8/anagrams.py
@@ -0,0 +1,46 @@
+def create(s):
+
+    characters = sorted(s)
+
+    result = []
+
+    cache = set()
+
+    working = ['']*len(s)
+
+    included = [False]*len(s)
+
+    def sequence(k):
+
+        if k == len(s):
+
+            candidate = "".join(working)
+
+            if candidate not in cache:
+
+                cache.add(candidate)
+
+                result.append(candidate)
+
+        else:
+
+            for iii in range(len(characters)):
+
+                if not included[iii]:
+
+                    included[iii] = True
+
+                    working[k] = characters[iii]
+
+                    sequence(k+1)
+
+                    included[iii] = False
+
+    sequence(0)
+
+    return result
+
+if __name__ == "__main__":
+    print(create("ab")) # [ab,ba]
+    print(create("abac")) # [aabc,aacb,abac,abca,acab,acba,baac,baca,bcaa,caab,caba,cbaa]
+    print(len(create("aybabtu"))) # 1260
---
+++ Viikko8/partition.py
@@ -0,0 +1,51 @@
+def check(t):
+
+    choice = [0]*len(t)
+    result = set()
+
+
+    if sum(t) % 2 != 0:
+
+        return False
+
+    def parts(k):
+
+        if k == len(t):
+
+            candidate = t.copy()
+
+            for iii in range(len(choice)):
+
+                candidate[iii] *= choice[iii]
+
+            if sum(t) == 2*sum(candidate):
+
+                result.add(True) # one answer is found and that is enough
+                return
+
+        else:
+
+            for iii in (0,1):
+
+                choice[k] = iii
+                parts(k+1)
+
+    
+    answer = parts(0)
+
+    if True in result:
+
+        return True
+
+    else:
+
+        return False
+
+
+if __name__ == "__main__":
+    print(check([3,4,5])) # False
+    print(check([16,8,4,4])) # True
+    print(check([9,4,8,7,6])) # True
+    print(check([1,2,3,4,5,6])) # False
+    print(check([1,2,3,4,5,6,7])) # True
+    print(check([4,4,4,6,6])) # True
