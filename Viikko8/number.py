bfd0819b5215e037e3d8895de413bc8a413ddb31
Matti Kantola <matti.kantola@helsinki.fi>
2022-11-04 15:58:21 +0200

    esitystavat vihdoin valmiina

--- Viikko8/number.py
+++ Viikko8/number.py
@@ -1,31 +1,27 @@
 def count(n):
 
-    cache = set()
     choice = [0]*(n+1)
+    suotuisia = [0]
 
-    def subsum(k,h):
+    def subsum(ylaraja, summa):
 
-        if h==n:
+        if summa == n:
 
-            cache.add(tuple(choice))
-
-        elif h > n:
-
-            return
+            suotuisia[0] += 1
 
         else:
 
-            for iii in range(1,n+1):
+            for iii in range(1, ylaraja+1):
 
-                if h + iii > n:
+                if summa + iii > n:
                     return
-                choice[iii] += 1
-                subsum(k+1, h+iii)
-                choice[iii] -= 1
-
-
-    subsum(0,0)
-    return len(cache)
+                else:
+                    choice[iii] += 1
+                    subsum(iii, summa+iii)
+                    choice[iii] -= 1
+    
+    subsum(n,0)
+    return suotuisia[0]
 
 if __name__ == "__main__":
     print(count(4)) # 5
